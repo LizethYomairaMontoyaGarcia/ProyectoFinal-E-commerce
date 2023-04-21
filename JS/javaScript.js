371310bc827a8acbcb0651e3344f773d32a4ccc5
lizeth montoya <lizethmontoyagarcia@gmail.com>
2023-04-21 16:09:36 -0500

    funcionalidad de filtrar por categoria y agregar a favoritos

--- JS/javaScript.js
+++ JS/javaScript.js
@@ -2,6 +2,7 @@
 let products = [];
 
 const URL_PRODUCTS = " http://localhost:3000/product";
+const URL_FAVORITE = " http://localhost:3000/favorite";
 
 const allCategoryProducts = async (url) => {
   const { data } = await axios.get(url);
@@ -24,116 +25,93 @@ function printImagsInHtml(container, printProducts) {
   container.innerHTML = "";
   printProducts.forEach((printProduct) => {
     container.innerHTML += `
-    <article class="container__imgs" data-imgs="imgs"  name=${printProduct.id}>
-    <img class="imgs__products" data-imgs="imgs" name=${printProduct.img} src=${printProduct.img}  alt=${printProduct.img} 
-    width="150px" height="150px">
+    <article class="container__imgs" data-imgs="imgs" name="${printProduct.id}">
+      <img
+        class="imgs__products"
+        data-imgs="imgs"
+        name="${printProduct.img}"
+        src="${printProduct.img}"
+        alt="${printProduct.img}"
+      />
 
-    <div class="product-box">
-              <div class="productImage">
-                <ul class="productOptionFavorite">
-                  <li>
-                    <button  >
-                      <img
-                        src="https://cdn-icons-png.flaticon.com/512/3722/3722014.png"
-                        alt=""
-                        width="22px"
-                      />
-                    </button>
-                  </li>
-                  <li>
-                    <button>
-                      <img
-                        src="https://e7.pngegg.com/pngimages/102/984/png-clipart-clockwise-rotation-arrow-symbol-logo-arrow-text-logo.png"
-                        alt=""
-                        width="22px"
-                      />
-                    </button>
-                  </li>
-                  <li>
-                    <button>
-                      <img
-                        src="https://cdn-icons-png.flaticon.com/512/1077/1077035.png"
-                        alt=""
-                        width="22px"
-                      />
-                    </button>
-                  </li>
-                </ul>
-              </div>
-              <div class="product-detail">
-                <a href="product-left-thumbnail.html" tabindex="0">
-                  <h6 class="nameProduct"> ${printProduct.name}
-                  </h6>
-                </a>
+      <div class="product-box">
+        <div class="productImage">
+          <ul class="productOptionFavorite">
+            <li>
+              <button class="favorite__products">
+                <img src="./imagenes/ojo.png" alt="ojo" width="23px" />
+              </button>
+            </li>
+            <li>
+              <button class="favorite__products">
+                <img src="./imagenes/retorno.png" alt="retorno" width="23px" />
+              </button>
+            </li>
+            <li>
+              <button
+                class="favorite__products"
+                data-attribute="favorite__products"
+                id="${printProduct.id}"
+              >
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/1077/1077035.png"
+                  alt="corazon"
+                  width="22px"
+                  data-attribute="favorite__products"
+                  id="${printProduct.id}"
+                />
+              </button>
+            </li>
+          </ul>
+        </div>
+        <div class="product-detail">
+          <p class="nameProduct"> <strong>${printProduct.name} </strong></p>
+          <p class="price_product"><strong>${printProduct.price}</strong></p>
 
-                <h5 class="sold text-content">
-                ${printProduct.price}
-                </h5>
+          <div class="product-rating mt-sm-2 mt-1">
+            <img
+              src="./imagenes/estrellistas.jpg"
+              alt="estrellistas"
+              width="70px"
+            />
+            <h6 class="stock">En Stock</h6>
+          </div>
 
-                <div class="product-rating mt-sm-2 mt-1">
-                  <ul class="rating">
-                    <li>
-                      <img src="https://img.freepik.com/vector-premium/icono-vector-estrella-simbolo-estrella-aislado-sobre-fondo-blanco-elemento-icono-plano-revision-calificacion-producto-cliente-aplicaciones-sitios-web-logros-juegos_646072-218.jpg?w=2000" 
-                      alt="" width="25px">
-                       </li>
-                    <li>
-                   <img src="https://img.freepik.com/vector-premium/icono-vector-estrella-simbolo-estrella-aislado-sobre-fondo-blanco-elemento-icono-plano-revision-calificacion-producto-cliente-aplicaciones-sitios-web-logros-juegos_646072-218.jpg?w=2000" 
-                   alt="" width="25px">
-                    </li>
-                    <li>
-                      <img src="https://png.pngtree.com/png-vector/20190725/ourlarge/pngtree-vector-star-icon-png-image_1577370.jpg" 
-                      alt="" width="26px">
-                       </li>
-                  </ul>
-
-                  <h6 class="theme-color">En Stock</h6>
-                </div>
-
-                <div class="add-to-cart-box">
-                  <div class="cart_qty qty-box">
-                    <div class="input-group">
-                      <button
-                        class="btn btn-add-cart addcart-button"
-                        tabindex="0"
-                      >
-                        Add
-                        <span class="add-icon">
-                          <i class="fa-solid fa-plus"></i>
-                        </span>
-                      </button>
-                      <button
-                        type="button"
-                        class="qty-left-minus"
-                        data-type="minus"
-                        data-field=""
-                        tabindex="0"
-                      >
-                        <i class="fa fa-minus" aria-hidden="true"></i>
-                      </button>
-                      <input
-                        class="form-control input-number qty-input"
-                        type="text"
-                        name="quantity"
-                        value="0"
-                        tabindex="0"
-                      />
-                      <button
-                        type="button"
-                        class="qty-right-plus"
-                        data-type="plus"
-                        data-field=""
-                        tabindex="0"
-                      >
-                        <i class="fa fa-plus" aria-hidden="true"></i>
-                      </button>
-                    </div>
-                  </div>
-                </div>
+          <div class="add-to-cart-box">
+            <div class="cart_qty qty-box">
+              <div class="input-group">
+                <button
+                  type="button"
+                  class="qty-left-minus"
+                  data-type="minus"
+                  data-field=""
+                  tabindex="0"
+                >
+                  <i class="fa fa-minus" aria-hidden="true"></i>
+                </button>
+                <input
+                  class="form-control input-number qty-input"
+                  type="text"
+                  name="quantity"
+                  value="0"
+                  tabindex="0"
+                />
+                <button
+                  type="button"
+                  class="qty-right-plus"
+                  data-type="plus"
+                  data-field=""
+                  tabindex="0"
+                >
+                  <i class="fa fa-plus" aria-hidden="true"></i>
+                </button>
               </div>
             </div>
-   
-                                         
-    </article>`;
+          </div>
+        </div>
+      </div>
+    </article>
+    `;
   });
 }
 
@@ -155,25 +133,20 @@ const findProductByName = (searchProduct, listProducts) => {
 };
 
 const searchForm = document.getElementById("search__products");
-
 searchForm.addEventListener("submit", (event) => {
   event.preventDefault();
-
   console.log(" imprimiendo al hijo ", searchForm.children);
 
   const formProducts = Array.from(searchForm.children);
-
   console.log("este es el formularioo", formProducts);
 
   const inputSearch = formProducts.find((item) => item.localName === "input");
-
   console.log(inputSearch.value);
 
   const searchProduct = inputSearch.value;
 
   if (searchProduct) {
     const response = findProductByName(searchProduct, products);
-
     console.log(response);
 
     if (response.messageSearch) {
@@ -188,179 +161,36 @@ searchForm.addEventListener("submit", (event) => {
 
 //-----------------------filtrar por categoria------------------------
 
-/*const getAllPosts = async () => {
-  const response = await fetch(url);
-  return await response.json();
-};
+document.addEventListener("click", (event) => {
+  const valueEvent = event.target.getAttribute("category");
 
-getAllPosts().then((response) => {
-  console.log("Mis productos:", response);
-  printImagsInHtml(mainImgsContainer, response);
+  if (valueEvent != null && valueEvent.includes("category")) {
+    const nameCategory = event.target.name;
+    const filterProductsCategory = products.filter((item) => {
+      return item.category.includes(nameCategory);
+    });
+    // console.log("productos filtrados", filterProductsCategory)
+    printImagsInHtml(mainImgsContainer, filterProductsCategory);
+  }
 });
 
-const mainImgsContainer = document.getElementById("main_imgs");
-console.log("Mi contenedor es:", mainImgsContainer);
+//------------------------agregar a favoritos------------------------
 
-function printImagsInHtml(container, printProducts) {
-  console.log("Estoy dentro de la funcion");
-
-  container.innerHTML = "";
-  printProducts.forEach((printProduct) => {
-    container.innerHTML += `
-      <article class="container__imgs" data-imgs="imgs"  name=${printProduct.id}>
-      <img class="imgs__products" data-imgs="imgs" name=${printProduct.img} src=${printProduct.img}  alt=${printProduct.img} 
-      width="150px" height="150px">
-
-      <div class="product-box">
-                <div class="productImage">
-                  <ul class="productOptionFavorite">
-                    <li>
-                      <button>
-                        <img
-                          src="https://cdn-icons-png.flaticon.com/512/3722/3722014.png"
-                          alt=""
-                          width="22px"
-                        />
-                      </button>
-                    </li>
-                    <li>
-                      <button>
-                        <img
-                          src="https://e7.pngegg.com/pngimages/102/984/png-clipart-clockwise-rotation-arrow-symbol-logo-arrow-text-logo.png"
-                          alt=""
-                          width="22px"
-                        />
-                      </button>
-                    </li>
-                    <li>
-                      <button>
-                        <img
-                          src="https://cdn-icons-png.flaticon.com/512/1077/1077035.png"
-                          alt=""
-                          width="22px"
-                        />
-                      </button>
-                    </li>
-                  </ul>
-                </div>
-                <div class="product-detail">
-                  <a href="product-left-thumbnail.html" tabindex="0">
-                    <h6 class="nameProduct"> ${printProduct.name}
-                    </h6>
-                  </a>
-
-                  <h5 class="sold text-content">
-                  ${printProduct.price}
-                  </h5>
-
-                  <div class="product-rating mt-sm-2 mt-1">
-                    <ul class="rating">
-                      <li>
-                        <img src="https://img.freepik.com/vector-premium/icono-vector-estrella-simbolo-estrella-aislado-sobre-fondo-blanco-elemento-icono-plano-revision-calificacion-producto-cliente-aplicaciones-sitios-web-logros-juegos_646072-218.jpg?w=2000" 
-                        alt="" width="25px">
-                         </li>
-                      <li>
-                     <img src="https://img.freepik.com/vector-premium/icono-vector-estrella-simbolo-estrella-aislado-sobre-fondo-blanco-elemento-icono-plano-revision-calificacion-producto-cliente-aplicaciones-sitios-web-logros-juegos_646072-218.jpg?w=2000" 
-                     alt="" width="25px">
-                      </li>
-                      <li>
-                        <img src="https://png.pngtree.com/png-vector/20190725/ourlarge/pngtree-vector-star-icon-png-image_1577370.jpg" 
-                        alt="" width="26px">
-                         </li>
-                    </ul>
-
-                    <h6 class="theme-color">En Stock</h6>
-                  </div>
-
-                  <div class="add-to-cart-box">
-                    <div class="cart_qty qty-box">
-                      <div class="input-group">
-                        <button
-                          class="btn btn-add-cart addcart-button"
-                          tabindex="0"
-                        >
-                          Add
-                          <span class="add-icon">
-                            <i class="fa-solid fa-plus"></i>
-                          </span>
-                        </button>
-                        <button
-                          type="button"
-                          class="qty-left-minus"
-                          data-type="minus"
-                          data-field=""
-                          tabindex="0"
-                        >
-                          <i class="fa fa-minus" aria-hidden="true"></i>
-                        </button>
-                        <input
-                          class="form-control input-number qty-input"
-                          type="text"
-                          name="quantity"
-                          value="0"
-                          tabindex="0"
-                        />
-                        <button
-                          type="button"
-                          class="qty-right-plus"
-                          data-type="plus"
-                          data-field=""
-                          tabindex="0"
-                        >
-                          <i class="fa fa-plus" aria-hidden="true"></i>
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-     
-                                           
-      </article>`;
-  });
-}*/
-
-/*busqueda de videos por nombres
-const filterNameCateryProducts = (termSearch, listProducts) => {
-  const filterProducts = listProducts.filter((video) =>
-    video.name.toLowerCase().includes(termSearch.toLowerCase())
-  );
-  const result = filterProducts.length ? filterProducts : listProducts;
-
-  const resultBusque = filterProducts.length ? false : "No existe este producto";
-
-  return {
-    resultSearch: result,
-    messageSearch: resultBusque,
-  };
-};
-
-const searchForm = document.getElementById("search__products");
-
-searchForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  console.log(searchForm.products);
-
-  const  formProducts = Array.from(searchForm.products);
-
-  const inputSearch = formProducts.find((item) => item.localName === "input");
-
-  console.log(inputSearch.value);
-
-  const searchProducts = inputSearch.value;
-
-  if (searchTermVideoTube) {
-    const searchResult = filterNameCateryProducts(searchProducts, mainImgsContainer)
-     
-    console.log(searchResult);
-
-    printImagsInHtml(mainImgsContainer, searchResult.resultSearch);
-
-    if (searchResult.messageSearch) {
-      Swal.fire("Oops!", searchResult.messageSearch, "error");
-    }
-  } else {
-    Swal.fire("Oops!", "No ingresaste ningun producto", "error");
+document.addEventListener("click", (event) => {
+  console.log(event);
+  const favorite = event.target.getAttribute("data-attribute");
+  if (favorite === "favorite__products") {
+    console.log(favorite);
+    const id = event.target.getAttribute("id");
+    const product = products.find((item) => item.id == id);
+    console.log("producto", product);
+    const aggregateProduct = async (url) => {
+      try {
+        await axios.post(url, product);
+      } catch (error) {
+        alert("Ocurrio un error");
+      }
+    };
+    aggregateProduct(URL_FAVORITE);
   }
-});*/
+});
